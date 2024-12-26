61b0a3166f9ba77fc1318c047aa9e25e13c7cb1f
Milanchuahan <miluchauhan1990@gmail.com>
2024-12-26 14:56:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# getmighty
+ 
---
+++ js/app.js
@@ -0,0 +1,116 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.prev'),
+          nextArrow: $('.next'),
+        responsive: [
+          {
+            breakpoint: 426,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll:1,
+            }
+          },
+        ]
+      });
+});
+
+$(document).ready(function(){
+  $('.slider2').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll:1,
+          }
+        },
+      ]
+    });
+});
+$(document).ready(function(){
+  $('.slider2').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll:1,
+          }
+        },
+      ]
+    });
+});
+
+$(document).ready(function(){
+  $('.slider2').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll:1,
+          }
+        },
+      ]
+    });
+});
+
+$(document).ready(function(){
+  $('.slider3').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll:1,
+          }
+        },
+      ]
+    });
+});
+$(document).ready(function(){
+  $('.slider4').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll:1,
+          }
+        },
+      ]
+    });
+});
+
+
+
